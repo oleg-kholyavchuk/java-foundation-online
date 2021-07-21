2448c3325d23836428ecfeb441daab9dae2d1df1
oleg-kholyavchuk <Oleg.Kholyavchuk@bk.ru>
2021-07-21 22:41:29 +0300

    "add Thread 19.07.21."

---
+++ src/main/java/ru/itsjava/threads/HomeThread.java
@@ -0,0 +1,30 @@
+package ru.itsjava.threads;
+
+import java.util.Scanner;
+
+public class HomeThread {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner temp = new Scanner(System.in);
+        String messange1 = temp.next();
+        String messange2 = temp.next();
+        System.out.println(messange1);
+        System.out.println(messange2);
+
+        PrinterThread printerThreadA = new PrinterThread(messange1, 5000L);
+
+        PrinterRunnable printerRunnable = new PrinterRunnable(messange2, 10000L);
+        Thread threadB = new Thread(printerRunnable);
+
+        System.out.println("start");
+
+        printerThreadA.start();
+        threadB.start();
+        threadB.join();
+
+        System.out.println("end");
+    }
+
+
+}
