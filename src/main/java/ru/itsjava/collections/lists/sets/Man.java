fff74420a7b5e0a75bee96f7efd00fcb88ebb9b2
oleg-kholyavchuk <Oleg.Kholyavchuk@bk.ru>
2021-07-24 19:44:59 +0300

    "add 24.07.21."

---
+++ src/main/java/ru/itsjava/collections/lists/sets/Man.java
@@ -0,0 +1,10 @@
+package ru.itsjava.collections.lists.sets;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Man {
+    private final String name;
+}
