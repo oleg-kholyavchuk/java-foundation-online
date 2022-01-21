72fe9976f506e272e12942adabf28205c7384777
oleg-kholyavchuk <Oleg.Kholyavchuk@bk.ru>
2022-01-21 18:56:47 +0300

    "add  21.01.22.Spring 1 degree. We write our own JUnit 2"

--- src/main/java/ru/itsjava/annotations/junit/MyFavoriteTest.java
+++ src/main/java/ru/itsjava/annotations/junit/MyFavoriteTest.java
@@ -2,22 +2,49 @@ package ru.itsjava.annotations.junit;
 
 public class MyFavoriteTest {
 
-    @Test
-    public void test1(){}
-
-    @Test
-    public void test2(){}
-
-    @Test
-    public void test3(){
-        throw new RuntimeException();
+    @BeforeAll
+    public void beforeAllMethod(){
+        System.out.println("Before All");
     }
 
     @Test
-    public void test4(){
-        throw new RuntimeException();
+    public void test1() {
     }
 
-    public void noTestMethod(){}
+//    @Test
+//    public void test2() { throw new RuntimeException();
+//    }
 
+    @AfterAll
+    public void test5() {
+        System.out.println("AfterAll");
+    }
+
+    @After
+    public void test6() {
+        System.out.println("After");
+    }
+
+    @Before
+    public void test7() {
+        System.out.println("Before");
+    }
+
+
+    @Test
+    public void test3() {
+    }
+
+//    @Test
+//    public void test4() {
+//        throw new RuntimeException();
+//    }
+
+    @DisplayName(message = "тест8")
+    public void test8() {
+    }
+
+    @DisplayName(message = "Тест Супер")
+    public void testSuper() {
+    }
 }
